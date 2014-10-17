a73da72a2bbaf35f1d7ad426be6e3f304ba457fb
carlislerainey <carlislerainey@gmail.com>
2014-10-17 06:59:25 -0400

    add rescaled data and tests for MCMC functions

---
+++ tests/testthat/test-sim-post-*.R
@@ -0,0 +1,24 @@
+
+context("sim_post_*")
+
+
+test_that("the MCMC function return no errors", {
+  data(politics_and_need)
+  data(politics_and_need_rescaled)
+  f <- oppose_expansion ~ gop_governor + percent_favorable_aca + percent_uninsured
+  post_jeffreys <- sim_post_jeffreys(f, data = politics_and_need, 
+                                     n_chains = 10,
+                                     n_burnin = 0,
+                                     n_sims = 100) 
+  post_gelman <- sim_post_gelman(f, data = politics_and_need_rescaled, 
+                                 n_chains = 10,
+                                 n_burnin = 0,
+                                 n_sims = 100) 
+  post_normal <- sim_post_normal(f, data = politics_and_need, sep_var = "gop_governor", 
+                                 n_chains = 10,
+                                 n_burnin = 0,
+                                 n_sims = 100) 
+  plot(post_jeffreys$mcmc_chains[, 2])
+  plot(post_gelman$mcmc_chains[, 2])
+  plot(post_normal$mcmc_chains[, 2])
+})
