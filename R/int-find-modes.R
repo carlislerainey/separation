beacc64a7d6c681c30f3f514715351a46a80cc45
carlislerainey <carlislerainey@gmail.com>
2014-10-18 17:30:52 -0400

    fix typo in find_modes() function

--- R/int-find-modes.R
+++ R/int-find-modes.R
@@ -1,10 +1,11 @@
 find_mode <- function(x) {
   dens <- density(x, n = 5000)
   mode <- dens$x[which(dens$y == max(dens$y))]
+  mode <- median(mode)
   return(mode)
 }
 
 find_modes <- function(X) {
   modes <- apply(X, 2, find_mode)
-  return(modes, n = 5000)
+  return(modes)
 }
